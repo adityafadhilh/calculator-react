e523fed245143e2c864259d32bfd2468fedba437
Aditya Fadhil Herindro <af.herindro@gmail.com>
2025-12-24 09:33:59 +0700

    adjust app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,16 @@ import CalculatorIcon from './assets/CalculatorIcon';
 import GraphIcon from './assets/GraphIcon';
 import MicIcon from './assets/MicIcon';
 
+type TabId = 'Calculator' | 'Graph' | 'Speech';
+
+type TabItemType = {
+  id: TabId;
+  label: string;
+  icon: React.ReactNode;
+};
+
 function App() {
-  const [isActiveTab, setIsActiveTab] = useState<'Calculator' | 'Graph' | 'Speech'>('Calculator');
+  const [isActiveTab, setIsActiveTab] = useState<TabId>('Calculator');
 
   useEffect(() => {
     document.body.style.zoom = "80%";
@@ -20,33 +28,41 @@ function App() {
     };
   }, []);
 
+  const tabs: TabItemType[] = [
+    {
+      id: 'Calculator',
+      label: 'Calculator',
+      icon: <CalculatorIcon className="h-8" color={isActiveTab == 'Calculator' ? '#fff' : '#000'} />
+    },
+    {
+      id: 'Graph',
+      label: 'Graph Visualizer',
+      icon: <GraphIcon className="h-8" color={isActiveTab == 'Graph' ? '#fff' : '#000'} />
+    },
+    {
+      id: 'Speech',
+      label: 'Speech Calculator',
+      icon: <MicIcon className="h-8" color={isActiveTab == 'Speech' ? '#fff' : '#000'} />
+    }
+  ];
+
   return (
     <>
       <div className='w-full mt-32 flex flex-col justify-center items-center'>
         <h1 className='text-5xl font-bold'>Calculator</h1>
         <p className='mt-4 mb-8'>Calculate, Visualize, and Speak Your Math</p>
         <div className='flex space-x-4'>
-          <div onClick={() => setIsActiveTab('Calculator')} className={`flex items-center justify-center w-fit md:w-48 p-2 text-center rounded-2xl font-bold cursor-pointer ${isActiveTab == 'Calculator' ?
-            'text-white bg-blue-600' :
-            'text-black bg-white border-2 border-gray-400'
-            } `}>
-            <CalculatorIcon className="h-8" color={isActiveTab == 'Calculator' ? '#fff' : '#000'} />
-            <p className='hidden md:inline'>Calculator</p>
-          </div>
-          <div onClick={() => setIsActiveTab('Graph')} className={`flex items-center justify-center w-fit md:w-48 p-2 text-center rounded-2xl font-bold cursor-pointer ${isActiveTab == 'Graph' ?
-            'text-white bg-blue-600' :
-            'text-black bg-white border-2 border-gray-400'
-            }`}>
-            <GraphIcon className="h-8" color={isActiveTab == 'Graph' ? '#fff' : '#000'} />
-            <p className='hidden md:inline'>Graph Visualizer</p>
-          </div>
-          <div onClick={() => setIsActiveTab('Speech')} className={`flex items-center justify-center w-fit md:w-48 p-2 text-center rounded-2xl font-bold cursor-pointer ${isActiveTab == 'Speech' ?
-            'text-white bg-blue-600' :
-            'text-black bg-white border-2 border-gray-400'
-            }`}>
-            <MicIcon className="h-8" color={isActiveTab == 'Speech' ? '#fff' : '#000'} />
-            <p className='hidden md:inline'>Speech Calculator</p>
-          </div>
+          {tabs.map((tab) => {
+            return (
+              <div onClick={() => setIsActiveTab(tab.id)} className={`flex items-center justify-center w-fit md:w-48 p-2 text-center rounded-2xl font-bold cursor-pointer ${isActiveTab == tab.id ?
+                'text-white bg-blue-600' :
+                'text-black bg-white border-2 border-gray-400'
+                } `}>
+                {tab.icon}
+                <p className='hidden md:inline'>{tab.label}</p>
+              </div>
+            )
+          })}
         </div>
       </div>
       <div className='flex w-full justify-center'>
